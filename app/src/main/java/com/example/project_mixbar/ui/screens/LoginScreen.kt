d513a7d3662786a3cc39d1be67b40480e30f193d
Hugo (Infor) Marcondes <143568062+inforgamer@users.noreply.github.com>
2026-04-09 18:06:37 -0300

    LoginScreen.kt

--- app/src/main/java/com/example/project_mixbar/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/project_mixbar/ui/screens/LoginScreen.kt
@@ -1,21 +1,27 @@
 package com.example.project_mixbar.ui.screens
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -148,9 +154,6 @@ fun LoginScreen() {
                         colors = ButtonDefaults.buttonColors(
                             containerColor = Color(0xff1E2438)
                         )
-
-
-
                         )
                     {
                         Text(
@@ -158,13 +161,57 @@ fun LoginScreen() {
                             color = Color(0xffb7b4b4)
                         )
                     }
+                    Spacer(modifier = Modifier.height(19.dp))
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color(0xffB7B4B4))
+                            .height(3.dp)
+                    )
+                    Spacer(modifier = Modifier.height(19.dp))
+
+                    Button(
+                        onClick = {
+                            print("text")
+                        },
+                        modifier = Modifier
+                            .height(48.dp)
+                            .fillMaxWidth(),
+
+                        shape = RoundedCornerShape(10.dp),
+                        border = BorderStroke(1.dp, Color(0xff525252)),
+
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xff1E2438)
+                        )
+                    )
+                    {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "LOGIn",
+
+                        )
+
+                        Text(
+                            text = "Entrar com Google",
+                            color = Color(0xffb7b4b4)
+                        )
+                    }
                 }
+
             }
+
+
+
 
 
         }
 
 }
+
+annotation class Icon
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
